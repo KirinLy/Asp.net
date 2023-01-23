56940877a252697894c2bc9b18ec2ca3db5aa3d4
Kirin Ly <nightfish9999@gmail.com>
2023-01-23 13:51:09 +0700

    Add /Chapter01/CodeAnalyzing

---
+++ Apps and Services with .NET 7/Chapter01/CodeAnalyzing/Program.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="Program.cs" company="NightFish">
+// Copyright (c) NightFish. All rights reserved.
+// </copyright>
+using System.Diagnostics;
+
+namespace CodeAnalyzing;
+
+/// <summary>
+/// Program.
+/// </summary>
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Debug.WriteLine("Hello, Debugger!");
+    }
+}
