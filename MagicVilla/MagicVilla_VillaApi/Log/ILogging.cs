09f15841ec9b2a7e2334e66c3964773ec1f136e9
Kirin Ly <nightfish9999@gmail.com>
2023-02-21 00:29:08 +0700

    Add custom log

---
+++ MagicVilla/MagicVilla_VillaApi/Log/ILogging.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_VillaApi.Log
+{
+    public interface ILogging
+    {
+        void LogInformation(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+    }
+}
---
+++ MagicVilla/MagicVilla_VillaApi/Log/LoggingV1.cs
@@ -0,0 +1,24 @@
+﻿namespace MagicVilla_VillaApi.Log
+{
+    public class LoggingV1 : ILogging
+    {
+        public void LogInformation(string message)
+        {
+            Console.WriteLine(DateTime.Now.ToString("s") + " " + message);
+        }
+
+        public void LogWarning(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(DateTime.Now.ToString("s") + " " + message);
+            Console.ResetColor();
+        }
+
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(DateTime.Now.ToString("s") + " " + message);
+            Console.ResetColor();
+        }
+    }
+}
